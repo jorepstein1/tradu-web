74017292fd101eb57bb619948e136ab47c2bfa6f
Jordan Epstein <jorepstein1@gmail.com>
2026-03-16 18:36:44 -0400

    Add a unittest for "tarada"

--- tests/test_word_reference_api.py
+++ tests/test_word_reference_api.py
@@ -35,6 +35,27 @@ def test_ball():
     assert len(translations[2].expressions) == 1
 
 
+def test_tarada():
+    with open("./tests/html_translation_samples/tarada.html", encoding="utf-8") as f:
+        response_content = f.read()
+    translations = word_reference.make_translations(response_content)
+    assert len(translations) == 6
+    assert translations[0].from_word.text == "tarado"
+    assert len(translations[0].to_words) == 1
+    assert len(translations[0].expressions) == 1
+    assert len(translations[1].to_words) == 3
+    assert len(translations[1].expressions) == 1
+    assert len(translations[2].to_words) == 4
+    assert len(translations[2].expressions) == 1
+    assert len(translations[3].to_words) == 2
+    assert len(translations[3].expressions) == 1
+    assert len(translations[4].to_words) == 2
+    assert len(translations[4].expressions) == 1
+    assert translations[5].from_word.text == "tarar"
+    assert len(translations[5].to_words) == 1
+    assert len(translations[5].expressions) == 1
+
+
 def test_cool():
     with open("./tests/html_translation_samples/cool.html", "r") as f:
         response_content = f.read()
