cf1f6cd0323d9714e874b437499cbbe2dcb3e6ea
Jordan Epstein <jorepstein1@gmail.com>
2026-03-20 13:45:30 -0400

    Add Vercel speed insights

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import { Tradu } from "@/components/Tradu";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 export default function Home() {
   return (
     <div className="min-h-screen bg-background text-foreground p-4">
       <Tradu />
       <Analytics />
+      <SpeedInsights />
     </div>
   );
 }
